debc90ccf8eb38ae4d8b7df60800358b6a755efe
Zach Harel <zach@zharel.me>
2026-07-24 16:07:36 -0400

    update for v2

---
+++ astro.config.mjs
@@ -0,0 +1,36 @@
+// @ts-check
+import { defineConfig } from 'astro/config';
+import starlight from '@astrojs/starlight';
+
+// https://astro.build/config
+export default defineConfig({
+	integrations: [
+		starlight({
+			title: 'NextFTC',
+			logo: {
+				light: './src/assets/nextftc-logo-light.png',
+				dark: './src/assets/nextftc-logo-dark.png',
+				replacesTitle: true,
+			},
+			favicon: '/favicon.png',
+			customCss: ['./src/styles/custom.css'],
+			social: [
+				{ icon: 'github', label: 'GitHub', href: 'https://github.com/NextFTC/NextFTCSuite' },
+				{ icon: 'discord', label: 'Discord', href: 'https://nextftc.dev/discord' },
+			],
+			sidebar: [
+				{
+					label: 'Guides',
+					items: [
+						// Each item here is one entry in the navigation menu.
+						{ label: 'Example Guide', slug: 'guides/example' },
+					],
+				},
+				{
+					label: 'Reference',
+					items: [{ autogenerate: { directory: 'reference' } }],
+				},
+			],
+		}),
+	],
+});
